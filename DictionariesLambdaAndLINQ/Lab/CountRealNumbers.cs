3f89336bf8403e8c40e0d2cd8b30378df08a5da1
Panaiot Uzunov <uzunov92@abv.bg>
2017-01-30 14:58:28 +0200

    Uploaded new console application.

---
+++ DictionariesLambdaAndLINQ/Lab/CountRealNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+
+            foreach (var num in nums)
+            {
+                if (counts.ContainsKey(num))
+                {
+                    counts[num]++;
+                }
+                else
+                {
+                    counts[num] = 1;
+                }
+            }
+
+            foreach (var key in counts)
+            {
+                Console.WriteLine("{0} -> {1}", key.Key, key.Value);
+            }
+        }
+    }
+}

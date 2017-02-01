352ebde52d9b647499de2f516fe00074bcedad74
Panaiot Uzunov <uzunov92@abv.bg>
2017-02-01 16:56:03 +0200

    Uploaded new console application.

---
+++ DictionariesLambdaAndLINQ/Lab/FoldAndSum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.OddOccurences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().ToLower().Split();
+            Dictionary<string, int> words = new Dictionary<string, int>();
+            List<string> result = new List<string>();
+
+            foreach (var word in input)
+            {
+                if (words.ContainsKey(word))
+                {
+                    words[word]++;
+                }
+                else
+                {
+                    words[word] = 1;
+                }
+            }
+
+            foreach (var pair in words)
+            {
+                if (pair.Value % 2 != 0)
+                {
+                    result.Add(pair.Key);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}

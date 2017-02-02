8616dd188031ddc74c76f668ff9528c587b0ad4c
Panaiot Uzunov <uzunov92@abv.bg>
2017-02-02 18:23:50 +0200

    Uploaded AMinerTask.cs (new console application)

---
+++ DictionariesLambdaAndLINQ/Exercises/AMinerTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.AMinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, int> resources = new Dictionary<string, int>();
+
+            while (!input.Equals("stop"))
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (resources.ContainsKey(input))
+                    resources[input] += quantity;
+                else
+                    resources[input] = quantity;
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var resource in resources)
+            {
+                Console.WriteLine("{0} -> {1}", resource.Key, resource.Value);
+            }
+        }
+    }
+}

92724c8ff5a8c829b29b680767aa7f16775f4955
Panaiot Uzunov <uzunov92@abv.bg>
2017-02-03 11:30:27 +0200

    Uploaded UserLogs.cs (new console application)

---
+++ DictionariesLambdaAndLINQ/Exercises/UserLogs.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.UserLogs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, Dictionary<string, int>> database =
+                new SortedDictionary<string, Dictionary<string, int>>();
+
+            string input = Console.ReadLine();
+
+            while (!input.Equals("end"))
+            {
+                string[] inputArgs = input.Split();
+                string user = inputArgs[2].Substring(5);
+                string ipAdress = inputArgs[0].Substring(3);
+
+                if (database.ContainsKey(user))
+                {
+                    if (database[user].ContainsKey(ipAdress))
+                    {
+                        database[user][ipAdress]++;
+                    }
+                    else
+                    {
+                        database[user][ipAdress] = 1;
+                    }
+                }
+                else
+                {
+                    database.Add(user, new Dictionary<string, int>());
+                    database[user].Add(ipAdress, 1);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            PrintUserAndIpAdresses(database);
+        }
+
+        private static void PrintUserAndIpAdresses(SortedDictionary<string, Dictionary<string, int>> database)
+        {
+            foreach (var item in database)
+            {
+                Console.WriteLine("{0}: ", item.Key);
+                Dictionary<string, int> ipAdresses = item.Value;
+
+                foreach (var pair in ipAdresses)
+                {
+                    string ip = pair.Key;
+                    int ipOccurrence = pair.Value;
+
+                    if (ip == ipAdresses.Keys.Last())
+                    {
+                        Console.Write("{0} => {1}.", pair.Key, pair.Value);
+                    }
+                    else
+                    {
+                        Console.Write("{0} => {1}, ", pair.Key, pair.Value);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

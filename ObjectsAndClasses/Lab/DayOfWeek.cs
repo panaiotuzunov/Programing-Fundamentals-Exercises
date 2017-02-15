256edba2455d4bc369868016d8c093b55234088f
Panaiot Uzunov <panaiot.uzunov@gmail.com>
2017-02-15 21:18:42 +0200

    Uploaded DayOfWeek.cs (new console application)

---
+++ ObjectsAndClasses/Lab/DayOfWeek.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _01.DayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date = DateTime.ParseExact(Console.ReadLine(), "d-M-yyyy", CultureInfo.InvariantCulture);
+            Console.WriteLine(date.DayOfWeek);
+        }
+    }
+}

af054138338c9562992438a8a323aa9c8175011b
Panaiot Uzunov <panaiot.uzunov@gmail.com>
2017-02-15 21:19:44 +0200

    Uploaded BigFactorial.cs (new console application)

---
+++ ObjectsAndClasses/Lab/BigFactorial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace _03.BigFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetBigFactorial(num));
+        }
+
+        private static BigInteger GetBigFactorial(int num)
+        {
+            BigInteger factorial = 1;
+
+            for (int i = 2; i <= num; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+    }
+}
